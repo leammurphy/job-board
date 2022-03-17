34725f5eff73f55ae7f6e9e94040034a7e8bab6b
Chulainn1 <leam.murphy1@gmail.com>
2022-03-16 23:37:26 -0600

    added a query for companies by id

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,7 @@
 const db = require('./db');
 
 const Query = {
+  company: (root, {id}) => db.companies.get(id),
   job: (root, {id}) => db.jobs.get(id), // pass gql args, parent val and args(the args from the gql query). return the specified job from the db using the id from args
   jobs: () => db.jobs.list(), // return arr of jobs from db  
 };
