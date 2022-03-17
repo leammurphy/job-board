4625bd6309715a0b3ba2a97a2a0504b04b9bb873
Chulainn1 <leam.murphy1@gmail.com>
2022-03-16 22:47:04 -0600

    jobDetail comp makes an http request that loads job data from the server using a loadjob function that accepts a param that is passed as a gql query var

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,5 +1,29 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
+export async function loadJob(id) {
+  const response = await fetch(endpointURL, {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({
+      query: `query JobQuery($id: ID!) {  
+        job(id: $id) {
+          id
+          title
+          company {
+            id
+            name
+          }
+          description
+        }
+      }`, 
+      variables: {id}
+      // the above query structure is required when passing a query a dynamic variable
+    })
+  });
+  const responseBody = await response.json();
+  return responseBody.data.job;
+}
+
 export async function loadJobs() { // call server and fetch jobs data
   const response = await fetch(endpointURL, {
     method: 'POST',
